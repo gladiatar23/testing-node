7ee05f91d139079c84d909113d32bdf82456e04b
AlexanderK <gladiatar23@gmail.com>
2018-10-14 23:37:34 +0300

    aaa

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 
 const sumFile = 12;
 const Path = require('path');
-
-const generetFiles= `node ../index.js -g ${sumFile} -o c:/42`;
-const watch ='node ../index.js -w c:/42/dirty'
-const cmd = process.platform == 'win32'? 'dir' : 'ls';
 const util = require('util');
 const os=require('os-utils');
 const memoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
@@ -17,23 +13,15 @@ let consoleWatch = fs.openSync("./consoleWatch.txt", "a");
 
 
 gTest= ()=>{
-   const cpG = spawn('node', ['../silverboltai-test-engineer-code-task-d3a40e01e814/index.js', '-g', sumFile, '-o', 'c:/42'],{stdio: [consoleWatch, consoleWatch, consoleWatch]});
-    //(stdout.search(sumFile)>-1)? console.log('Pass') : console.log('Fail');
-    os.cpuUsage(function(cpuUsage){
-        console.log(`${Date.now()} CPU usege: ${Math.round (cpuUsage * 100)/100}%\nMemory Usege: ${Math.round (memoryUsed * 100)/100} mb`);
-    });
+   let cpG = spawn('node', ['../silverboltai-test-engineer-code-task-d3a40e01e814/index.js',
+       '-g', sumFile,
+       '-o', 'c:/42'],
+       {stdio: [consoleWatch, consoleWatch, consoleWatch]});
     setTimeout(function() {
         // console.table(listOfObj,['a','b','c']);
         console.log('Test end');
-        console.log('json'+JSON.stringify(listOfObj,null, 4));
-        // console.log('util'+ util.inpect(listOfObj , ({breakLength: Infinity}));
-        console.log('SUKA\n'+ util.inspect(listOfObj, { compact: true, depth: Infinity, breakLength: Infinity }, { showHidden: true }));
-        // listOfObj.items.map(function (item) {
-        //     console.log(listOfObj.item.cid);
-        // });
-        console.log(JSON.stringify(listOfObj));
-        cpG.kill();
-    }, 30000);
+       console.log(JSON.stringify(listOfObj));
+    }, 109000);
 
 };
 
@@ -41,18 +29,19 @@ let result = {};
 wTest =()=>{
 let cpW = spawn('node', ['../silverboltai-test-engineer-code-task-d3a40e01e814/index.js', '-w', 'c:/42'], {stdout: [consoleWatch, consoleWatch, result]});
     cpW.stdout.on('data', function(data) {
+        os.cpuUsage(function(cpuUsage){
+            console.log(`${Date.now()},${Math.round(cpuUsage * 100)/100},${Math.round (memoryUsed * 100)/100}`);
+           spliter(data.toString(),cpuUsage) ;
+            //spliter(cpuUsage,memoryUsed) ;
 
-        dataToObj = spliter(data.toString()) ;
-    });
-    os.cpuUsage(function(cpuUsage){
-        console.log(`${Date.now()} CPU usege: ${Math.round (cpuUsage * 100)/100}%\nMemory Usege: ${Math.round (memoryUsed * 100)/100} mb`);
-        setTimeout(function() {
+            // console.log(`${Date.now()} CPUusege: ${Math.round (cpuUsage * 100)/100}% MemoryUsege: ${Math.round (memoryUsed * 100)/100}mb`);
+        });
+        });
+
+    setTimeout(function() {
     console.log('End of the Watcher');
-
     cpW.kill();
-}, 19999);
-
-    });
+}, 100000);
 }
 
 
@@ -60,16 +49,15 @@ let listOfObj = [];
 function toObj(arr){
 
     let singleObj = {};
-    //singleObj['line'] = i;
-    //singleObj.line = i;
     for (let i = 0, j=0; i < arr.length ; ++i, j++)
         if ( (arr[i] !== undefined) && (arr[i] !== '::!') && (arr[i] !== '!::') ){
-            //singleObj[j]=arr[i] ;
+            singleObj.startTime ;
+            singleObj.cpuProc;
+            singleObj.memoryProc;
             singleObj.filePath = upath.normalize(arr[arr.length - 1]);
             singleObj.time = arr[0];
             singleObj.func = arr[(arr.length-1) /2];
             console.log(singleObj);
-
         }
         else --j;
     console.log(singleObj);
@@ -77,7 +65,6 @@ function toObj(arr){
     // const category = [arr];
     //
     // category.map(function (arr) {
-
     //     singleObj['filePath'] =upath.normalize(arr[arr.length - 1]);
     //     singleObj['time'] = arr[0];
     //     singleObj['func'] = arr[(arr.length-1) /2];
@@ -93,11 +80,39 @@ function toObj(arr){
 // foreach(obj in list)
 // {
 //     action = obj.func;
-//     if (action == "CLEAN") {
-//         continue;
-//     } else if (action == "DIRTY") {
-//        doDirtyTest();
-//     } else if(action == "SOME"){
+//     time = obj.time;
+//     pathToChack = obj.filePath;
+// let addCounter = 0;
+//
+//     switch (action) {
+//
+//         case "ADDED":
+//             fs.existsSync(pathToChack) ? addCounter++ : null;
+//
+//             break;
+//
+//         case "CHANGED":
+//
+//             break;
+//
+//         case "DIRTY":
+//             let fileExt = Path.extname(path);
+//             if (fileExt === '.dirty'){
+//
+//             }
+//
+//             break;
+//         // case REMOVED:
+//         //
+//         // break;
+//
+//         case "CLEAN":
+//
+//             break;
+//
+//         case "ERROR":
+//
+//             break;
 //
 //     }
 // }
@@ -105,16 +120,19 @@ function toObj(arr){
 
 
 
-
 wTest();
-gTest();
+setTimeout(function() {
+    console.log('generate');
+    gTest();
+}, 190);
+
 
 function spliter(strout) {
-    let splitAdress = toObj(strout.split(/(::!)|(!::)/));
-
-    // console.log(splitAdress.cid);
-
+    toObj(strout.split(/(::!)|(!::)/));
 }
+
+
+
 //
 // console.log(splitAdress[splitAdress.length - 1]);
 // console.log(splitAdress[(splitAdress.length-1) /2]);
@@ -126,14 +144,5 @@ function spliter(strout) {
 //     console.log("SUKA");
 // }
 // else (console.log("eshyo bolshe"))
-
-
-
-
 // console.log("splitAdress:"+splitAdress.filter('c:'));
 // console.log(splitOper);
-
-
-
-
-
