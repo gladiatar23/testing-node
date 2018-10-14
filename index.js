b3ce82653e8298db55d5d38a81e5765580ad53a2
AlexanderK <gladiatar23@gmail.com>
2018-10-14 15:21:24 +0300

    obj fix
    obj fix

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const Path = require('path');
 const generetFiles= `node ../index.js -g ${sumFile} -o c:/42`;
 const watch ='node ../index.js -w c:/42/dirty'
 const cmd = process.platform == 'win32'? 'dir' : 'ls';
-const util = require('util')
+const util = require('util');
 const os=require('os-utils');
 const memoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
-//let now = new Date().getTime();
 let spawn=require('child_process').spawn;
 const fs = require('fs');
 const upath = require('upath');
@@ -24,12 +23,17 @@ gTest= ()=>{
         console.log(`${Date.now()} CPU usege: ${Math.round (cpuUsage * 100)/100}%\nMemory Usege: ${Math.round (memoryUsed * 100)/100} mb`);
     });
     setTimeout(function() {
-        debugger;
         // console.table(listOfObj,['a','b','c']);
-        // console.log('Test');
-        // console.log('json'+JSON.stringify({listOfObj}));
+        console.log('Test end');
+        console.log('json'+JSON.stringify(listOfObj,null, 4));
+        // console.log('util'+ util.inpect(listOfObj , ({breakLength: Infinity}));
+        console.log('SUKA\n'+ util.inspect(listOfObj, { compact: true, depth: Infinity, breakLength: Infinity }, { showHidden: true }));
+        // listOfObj.items.map(function (item) {
+        //     console.log(listOfObj.item.cid);
+        // });
+        console.log(JSON.stringify(listOfObj));
         cpG.kill();
-    }, 10000);
+    }, 30000);
 
 };
 
@@ -37,7 +41,7 @@ let result = {};
 wTest =()=>{
 let cpW = spawn('node', ['../silverboltai-test-engineer-code-task-d3a40e01e814/index.js', '-w', 'c:/42'], {stdout: [consoleWatch, consoleWatch, result]});
     cpW.stdout.on('data', function(data) {
-        // console.log(data.toString());
+
         dataToObj = spliter(data.toString()) ;
     });
     os.cpuUsage(function(cpuUsage){
@@ -46,13 +50,30 @@ let cpW = spawn('node', ['../silverboltai-test-engineer-code-task-d3a40e01e814/i
     console.log('End of the Watcher');
 
     cpW.kill();
-}, 9999);
+}, 19999);
 
     });
 }
-let listOfObj = {};
+
+
+let listOfObj = [];
 function toObj(arr){
-    let singleObj =[];
+
+    let singleObj = {};
+    //singleObj['line'] = i;
+    //singleObj.line = i;
+    for (let i = 0, j=0; i < arr.length ; ++i, j++)
+        if ( (arr[i] !== undefined) && (arr[i] !== '::!') && (arr[i] !== '!::') ){
+            //singleObj[j]=arr[i] ;
+            singleObj.filePath = upath.normalize(arr[arr.length - 1]);
+            singleObj.time = arr[0];
+            singleObj.func = arr[(arr.length-1) /2];
+            console.log(singleObj);
+
+        }
+        else --j;
+    console.log(singleObj);
+    return listOfObj.push(singleObj);
     // const category = [arr];
     //
     // category.map(function (arr) {
@@ -66,15 +87,21 @@ function toObj(arr){
     //
     // })
 
-
-    for (let i = 0, j=0; i < arr.length ; ++i, j++)
-        if ( (arr[i] !== undefined) && (arr[i] !== '::!') && (arr[i] !== '!::') ){
-            singleObj[j]=arr[i] ;
-    }
-        else --j;
-    console.log(singleObj);
-        return listOfObj+=singleObj;
 }
+
+// list = toObj();
+// foreach(obj in list)
+// {
+//     action = obj.func;
+//     if (action == "CLEAN") {
+//         continue;
+//     } else if (action == "DIRTY") {
+//        doDirtyTest();
+//     } else if(action == "SOME"){
+//
+//     }
+// }
+
 
 
 
@@ -84,7 +111,8 @@ gTest();
 
 function spliter(strout) {
     let splitAdress = toObj(strout.split(/(::!)|(!::)/));
-    // console.log(splitAdress);
+
+    // console.log(splitAdress.cid);
 
 }
 //
@@ -105,26 +133,7 @@ function spliter(strout) {
 // console.log("splitAdress:"+splitAdress.filter('c:'));
 // console.log(splitOper);
 
-//var seconds = process.argv[2] || 2;
 
-//wTest.on('exit', () => { console.log('Stopped'); clearTimeout(timeout); });
-
-// let test2 = cp.spawnSync('node', ['../silverboltai-test-engineer-code-task-d3a40e01e814/index.js', '-w', 'c:/42'], {stdio: [consoleWatch, consoleWatch, consoleWatch]});
-// os.cpuUsage(function(cpuUsage){
-//     console.log(`${now} CPU usege: ${Math.round (cpuUsage * 100)/100}%\nMemory Usege: ${Math.round (memoryUsed * 100)/100} mb`);
-// });
-//
-// const lineReader = require('readline').createInterface({
-//     input: require('fs').createReadStream("./consoleWatch.txt")
-// });
-
-// lineReader.on('line', function (line) {
-//     console.log('Line from file:', line);
-// });
-
-
-// const test1= exec(generetFiles, testCommand);
-//const test2=  exec(watch, testCommand);
 
 
 
